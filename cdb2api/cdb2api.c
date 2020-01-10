07cbaefbc0af41dde3cd02e04f890aaaa01697e4
Rivers Zhang <hzhang320@bloomberg.net>
2020-01-10 20:15:51 +0000

    dddd

--- cdb2api/cdb2api.c
+++ cdb2api/cdb2api.c
@@ -923,7 +923,7 @@ static cdb2_ssl_sess_list cdb2_ssl_sess_cache;
    4096 txn/us (~4 billion transactions per second) till September 17, 2112.
 
    See next_cnonce() for details. */
-#define CNONCE_STR_FMT "%lx-%x-%llx-"
+#define CNONCE_STR_FMT "-%lx-%x-%llx"
 #define CNONCE_STR_SZ 52 /* 8 + 1 + 8 + 1 + 16 + 1 + 16 + 1 (NUL) */
 
 #define CNT_BITS 12
@@ -3507,8 +3507,8 @@ static int next_cnonce(cdb2_hndl_tp *hndl)
             c->hostid = _MACHINE_ID;
             c->pid = _PID;
             c->hndl = hndl;
-            c->ofs = sprintf(c->str, CNONCE_STR_FMT, c->hostid, c->pid,
-                             (unsigned long long)c->hndl);
+            c->ofs = sprintf(c->str + (sizeof(c->seq) << 1),
+                    CNONCE_STR_FMT, c->hostid, c->pid, (unsigned long long)c->hndl);
         }
         c->seq = (now << CNT_BITS);
     } else {
@@ -3516,16 +3516,15 @@ static int next_cnonce(cdb2_hndl_tp *hndl)
     }
 
     if (rc == 0) {
-        in = (char *)&c->seq;
-        end = in + sizeof(c->seq);
-        out = c->str + c->ofs;
+        end = (char *)&c->seq;
+        in = end + sizeof(c->seq);
+        out = c->str;
 
         while (in != end) {
-            char i = *(in++);
+            char i = *(--in);
             *(out++) = hex[(i & 0xf0) >> 4];
             *(out++) = hex[i & 0x0f];
         }
-        *out = 0;
     }
     return rc;
 }
